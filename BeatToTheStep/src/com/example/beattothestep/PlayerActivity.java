daa1b38bed86aea265d885ad8606bb436b6835eb
Wade Burch <wade@wadeburch.com>
2014-03-23 23:46:27 -0500

    NDK sample JNI foundation routines for playback control.

--- BeatToTheStep/src/com/example/beattothestep/PlayerActivity.java
+++ BeatToTheStep/src/com/example/beattothestep/PlayerActivity.java
@@ -2,9 +2,15 @@ package com.example.beattothestep;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.AssetManager;
 import android.view.Menu;
 
 public class PlayerActivity extends Activity {
+	
+	static final int CLIP_NONE = 0;
+	
+	static boolean isPlayingAsset = false;
+    static boolean isPlayingUri = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +25,52 @@ public class PlayerActivity extends Activity {
 		return true;
 	}
 
+	/** Called when the activity is about to be destroyed. */
+    @Override
+    protected void onPause()
+    {
+        // turn off all audio
+        selectClip(CLIP_NONE, 0);
+        isPlayingAsset = false;
+        setPlayingAssetAudioPlayer(false);
+        isPlayingUri = false;
+        setPlayingUriAudioPlayer(false);
+        super.onPause();
+    }
+
+    /** Called when the activity is about to be destroyed. */
+    @Override
+    protected void onDestroy()
+    {
+        shutdown();
+        super.onDestroy();
+    }
+
+    /** Native methods, implemented in jni folder */
+    
+    // Basic functions from NDK sample.
+    public static native void createEngine();
+    public static native void createBufferQueueAudioPlayer();
+    public static native boolean createAssetAudioPlayer(AssetManager assetManager, String filename);
+    public static native void setPlayingAssetAudioPlayer(boolean isPlaying);
+    public static native boolean createUriAudioPlayer(String uri);
+    public static native void setPlayingUriAudioPlayer(boolean isPlaying);
+    public static native void setLoopingUriAudioPlayer(boolean isLooping);
+    public static native void setChannelMuteUriAudioPlayer(int chan, boolean mute);
+    public static native void setChannelSoloUriAudioPlayer(int chan, boolean solo);
+    public static native int getNumChannelsUriAudioPlayer();
+    public static native void setVolumeUriAudioPlayer(int millibel);
+    public static native void setMuteUriAudioPlayer(boolean mute);
+    public static native void enableStereoPositionUriAudioPlayer(boolean enable);
+    public static native void setStereoPositionUriAudioPlayer(int permille);
+    public static native boolean selectClip(int which, int count);
+    public static native void shutdown();
+    
+    // TODO Added functions for time stretching.
+
+    /** Load jni .so on initialization */
+    static {
+         System.loadLibrary("native-audio-jni");
+    }
+    
 }
