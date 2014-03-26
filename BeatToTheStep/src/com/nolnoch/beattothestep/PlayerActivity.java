eaccda99da036a6a96595f7b9526c5172f9531c6
Wade Burch <wade@wadeburch.com>
2014-03-26 03:02:43 -0500

    Added EchoNest 3rd Party library for BPM detection.

--- BeatToTheStep/src/com/nolnoch/beattothestep/PlayerActivity.java
+++ BeatToTheStep/src/com/nolnoch/beattothestep/PlayerActivity.java
@@ -1,5 +1,7 @@
 package com.nolnoch.beattothestep;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -17,15 +19,22 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.echonest.api.v4.EchoNestAPI;
+import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.Track;
+import com.echonest.api.v4.TrackAnalysis;
+
 
 
 public class PlayerActivity extends Activity {
 
+	private static final int ONE_SECOND = 1000;
+	private static final int DEFAULT_RATE = 1000;
 	private static final int NUM_TIME_STEPS = 15;
-	private static final int TIME_INTERVAL = 60 / NUM_TIME_STEPS * 1000;
-	private static final int MIN_SPM = 30;
-	private static final int MAX_SPM = 180;
+	private static final int TIME_INTERVAL = 60 / NUM_TIME_STEPS * ONE_SECOND;
 	private static final int SETTINGS_RC = 1080;
+	private static final double MIN_SPM = 30.0d;
+	private static final double MAX_SPM = 180.0d;
 	private static final String DEBUG = "DEBUG";
 
 	static final int CLIP_NONE = 0;
@@ -36,8 +45,14 @@ public class PlayerActivity extends Activity {
 	private ArrayList<Long> stepStart;
 	private long stepCurrent, stepDelta;
 	private int tick;
-	static int spm;
+	static double spm, bpm;
 	static int spm_old, spm_delta;
+	
+	private String enAPIKey = "AWC7MGBH4CCN9Z8QX"; 
+	private static EchoNestAPI en;
+	private TrackAnalysis trAnal;
+	private AssetManager am;
+	private String demo_song;
 
 	private TimerTask stepTask;
 	private Timer stepTimer;
@@ -54,11 +69,39 @@ public class PlayerActivity extends Activity {
 		stepStart = new ArrayList<Long>(NUM_TIME_STEPS);
 		tick = 0;
 
+		initEchoNestAPI();
+		try {
+			loadAudioAsset();
+		} catch (EchoNestException e) {
+			Log.d(DEBUG, "EchoNest Error: " + e);
+		}
 		createStepTimer();
 		createStepSensor();
 		
 		// XXX Call this on PLAY.
 		initStepEngine();
+	}
+	
+	private void initEchoNestAPI() {
+		en = new EchoNestAPI(enAPIKey);
+	}
+	
+	private void loadAudioAsset() throws EchoNestException {
+		am = getAssets();
+		File song = new File(demo_song);
+		
+		try {
+			Track tr = en.uploadTrack(song);
+			tr.waitForAnalysis(ONE_SECOND * 10);
+			if (tr.getStatus() == Track.AnalysisStatus.COMPLETE) {
+				trAnal = tr.getAnalysis();
+				bpm = trAnal.getTempo();
+			} else {
+				Log.d(DEBUG, "Track Status Error: " + tr.getStatus());
+			}
+		} catch (IOException e) {
+			Log.d(DEBUG, "IO Error: " + e);
+		}
 	}
 
 	private void createStepTimer() {
@@ -69,17 +112,24 @@ public class PlayerActivity extends Activity {
 				if (stepStart.get(tick) == 0) {
 					stepStart.set(tick, stepCurrent);
 				} else {
+					int newRate;
+					
 					stepDelta = stepCurrent - stepStart.get(tick);
-					spm = (int) stepDelta / 60;
+					spm = stepDelta / 60.0d;
 					spm = Math.max(MIN_SPM, Math.min(MAX_SPM, spm));
 					Log.d(DEBUG, "Current SPM: " + spm);
+					
+					newRate = (int) ((spm / bpm) * DEFAULT_RATE);
+					
+					setPlaybackRate(newRate);
 
-					// Until beat detection is working, use variable step rate.
+					/* Until beat detection is working, use variable step rate.
 					if (spm_old != 0) {
 						spm_delta = (spm - spm_old) / spm * 1000 + 1000;
 						setPlaybackRate(spm_delta);
 					}
 					spm_old = spm;
+					*/
 				}
 
 				if (tick++ >= NUM_TIME_STEPS) {
