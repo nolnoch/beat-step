ac889712e9d731440c304cb749a07f5315d35f9b
Wade Burch <wade@wadeburch.com>
2014-03-26 21:01:27 -0500

    Layouts and resources refactored to fit Google Best Practices.

--- BeatToTheStep/src/com/nolnoch/beattothestep/SongSelectActivity.java
+++ BeatToTheStep/src/com/nolnoch/beattothestep/SongSelectActivity.java
@@ -1,7 +1,14 @@
 package com.nolnoch.beattothestep;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SongSelectActivity extends Activity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_songs);
+	}
+	
 }
